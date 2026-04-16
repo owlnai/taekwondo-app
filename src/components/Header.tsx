349e287d400a8e06b9ae77d506509b4d718e5d29
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2026-04-16 00:56:50 -0400

    feat(Header): add back button for subroutes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
-import { Calendar } from 'lucide-react';
-import { Link, NavLink, useLocation } from 'react-router-dom';
+import { ArrowLeft, Calendar } from 'lucide-react';
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { ProfileAvatar } from './ProfileAvatar';
+import { Button } from '@/common/Button';
 
 const headerItems = [
   {
@@ -21,14 +22,36 @@ const headerItems = [
   },
 ];
 
+function BackButton(props: React.ComponentProps<typeof Button>) {
+  const navigate = useNavigate();
+  return (
+    <Button
+      variant="ghost"
+      size="icon-sm"
+      onClick={() => navigate(-1)}
+      {...props}
+    >
+      <ArrowLeft size={16} />
+    </Button>
+  );
+}
+
 export function Header() {
   const location = useLocation();
-  const title = headerItems.find((h) =>
-    location.pathname.startsWith(h.to)
-  )?.label;
+  const title =
+    headerItems.find((h) => location.pathname.startsWith(h.to))?.label ??
+    'Exámenes';
+  const isSubRoute =
+    location.pathname === '/'
+      ? false
+      : headerItems.every((h) => location.pathname !== h.to);
+
   return (
     <header className="relative z-40 flex items-center justify-between flex-none my-4 safe-area-top">
-      <h1 className="font-semibold">{title ?? 'Exámenes'}</h1>
+      <div className="flex items-center gap-1">
+        {isSubRoute ? <BackButton className="-ml-2" /> : null}
+        <h1 className="font-semibold">{title}</h1>
+      </div>
 
       <div className="flex items-center gap-6">
         <NavLink
